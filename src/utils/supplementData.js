0adba6477d25f530aca141032187316e28ef96bb
Jeff Sahim <jeffreysahim@gmail.com>
2023-04-19 17:18:42 -0400

    Add API fetch and data cleaning functionality

---
+++ src/utils/supplementData.js
@@ -0,0 +1,174 @@
+const supplementData = 
+  [
+    {
+      name: "Black",
+      image: "/new/path",
+      oneSize: null,
+      smallPrice: 4.5,
+      mediumPrice: 5.5,
+      largePrice: 6.5
+    },{
+      name: "Latte",
+      image: "/new/path",
+      oneSize: null,
+      smallPrice: 4.5,
+      mediumPrice: 5.5,
+      largePrice: 6.5
+    },{
+      name: "Cappuccino",
+      image: "/new/path",
+      oneSize: null,
+      smallPrice: 4.5,
+      mediumPrice: 5.5,
+      largePrice: 6.5
+    },{
+      name: "Americano",
+      image: "/new/path",
+      oneSize: null,
+      smallPrice: 4.5,
+      mediumPrice: 5.5,
+      largePrice: 6.5
+    },{
+      name: "Espresso",
+      image: "/new/path",
+      oneSize: 5.5,
+      smallPrice: null,
+      mediumPrice: null,
+      largePrice: null
+    },{
+      name: "Doppio",
+      image: "/new/path",
+      oneSize: 5.5,
+      smallPrice: null,
+      mediumPrice: null,
+      largePrice: null
+    },{
+      name: "Cortado",
+      image: "/new/path",
+      oneSize: 5.5,
+      smallPrice: null,
+      mediumPrice: null,
+      largePrice: null
+    },{
+      name: "Red Eye",
+      image: "/new/path",
+      oneSize: null,
+      smallPrice: 4.5,
+      mediumPrice: 5.5,
+      largePrice: 6.5
+    },{
+      name: "Galão",
+      image: "/new/path",
+      oneSize: null,
+      smallPrice: 4.5,
+      mediumPrice: 5.5,
+      largePrice: 6.5
+    },{
+      name: "Lungo",
+      image: "/new/path",
+      oneSize: null,
+      smallPrice: 4.5,
+      mediumPrice: 5.5,
+      largePrice: 6.5
+    },{
+      name: "Mocha",
+      image: "/new/path",
+      oneSize: null,
+      smallPrice: 4.5,
+      mediumPrice: 5.5,
+      largePrice: 6.5
+    },{
+      name: "Macchiato",
+      image: "/new/path",
+      oneSize: null,
+      smallPrice: 4.5,
+      mediumPrice: 5.5,
+      largePrice: 6.5
+    },{
+      name: "Ristretto",
+      image: "/new/path",
+      oneSize: null,
+      smallPrice: 4.5,
+      mediumPrice: 5.5,
+      largePrice: 6.5
+    },{
+      name: "Flat White",
+      image: "/new/path",
+      oneSize: null,
+      smallPrice: 4.5,
+      mediumPrice: 5.5,
+      largePrice: 6.5
+    },{
+      name: "Affogato",
+      image: "/new/path",
+      oneSize: null,
+      smallPrice: 4.5,
+      mediumPrice: 5.5,
+      largePrice: 6.5
+    },{
+      name: "Café au Lait",
+      image: "/new/path",
+      oneSize: null,
+      smallPrice: 4.5,
+      mediumPrice: 5.5,
+      largePrice: 6.5
+    },{
+      name: "Irish",
+      image: "/new/path",
+      oneSize: null,
+      smallPrice: 4.5,
+      mediumPrice: 5.5,
+      largePrice: 6.5
+    },{
+      name: "Guayoyo",
+      image: "/new/path",
+      oneSize: null,
+      smallPrice: 4.5,
+      mediumPrice: 5.5,
+      largePrice: 6.5
+    },{
+      name: "Iced Coffee",
+      image: "/new/path",
+      oneSize: null,
+      smallPrice: 4.5,
+      mediumPrice: 5.5,
+      largePrice: 6.5
+    },{
+      name: "Iced Espresso",
+      image: "/new/path",
+      oneSize: null,
+      smallPrice: 4.5,
+      mediumPrice: 5.5,
+      largePrice: 6.5
+    },{
+      name: 'Cold Brew',
+      image: "/new/path",
+      oneSize: null,
+      smallPrice: 4.5,
+      mediumPrice: 5.5,
+      largePrice: 6.5
+    },{
+      name: "Frappuccino",
+      image: "/new/path",
+      oneSize: null,
+      smallPrice: 4.5,
+      mediumPrice: 5.5,
+      largePrice: 6.5
+    },{
+      name: "Nitro",
+      image: "/new/path",
+      oneSize: null,
+      smallPrice: 4.5,
+      mediumPrice: 5.5,
+      largePrice: 6.5
+    },{
+      name: "Mazagran",
+      image: "/new/path",
+      oneSize: null,
+      smallPrice: 4.5,
+      mediumPrice: 5.5,
+      largePrice: 6.5
+    }
+  ]
+
+  export default supplementData
---
+++ src/utils/utilities.js
@@ -0,0 +1,44 @@
+import supplementData from "./supplementData"
+
+const cleanDrinkData = (hotData, coldData) => {
+  let cleanHotData, cleanIcedData
+  if(hotData){
+    cleanHotData = hotData.map(drink => {
+      const foundHotDrink = supplementData.find(bev => bev.name === drink.title)
+      return {
+                name: drink.title,
+                description: drink.description, 
+                ingredients: drink.ingredients,
+                type: "hot", 
+                image: foundHotDrink.image,
+                oneSize: foundHotDrink.oneSize,
+                smallPrice: foundHotDrink.smallPrice,
+                mediumPrice: foundHotDrink.mediumPrice,
+                largePrice: foundHotDrink.largePrice
+              }
+    })
+  } 
+  if(coldData){
+    cleanIcedData = coldData.map(drink => {
+      const foundColdDrink = supplementData.find(bev => bev.name === drink.title)
+      return {
+                name: drink.title,
+                description: drink.description, 
+                ingredients: drink.ingredients,
+                type: "iced", 
+                image: foundColdDrink.image,
+                oneSize: foundColdDrink.oneSize,
+                smallPrice: foundColdDrink.smallPrice,
+                mediumPrice: foundColdDrink.mediumPrice,
+                largePrice: foundColdDrink.largePrice
+              }
+    })
+  }
+  const joinedData = cleanHotData.concat(cleanIcedData)
+  joinedData.forEach(datum => {
+    datum['id'] = joinedData.indexOf(datum) + 1
+  })
+  return joinedData
+}
+
+export default cleanDrinkData
