e0bae593057f521b5f715d6e360e9c5262d0e02d
Jeff Sahim <jeffreysahim@gmail.com>
2023-04-19 17:49:09 -0400

    Update supplementData pricing

--- src/utils/supplementData.js
+++ src/utils/supplementData.js
@@ -4,48 +4,48 @@ const supplementData =
       name: "Black",
       image: "/new/path",
       oneSize: null,
-      smallPrice: 4.5,
-      mediumPrice: 5.5,
-      largePrice: 6.5
+      smallPrice: 2.65,
+      mediumPrice: 3.65,
+      largePrice: 4.65
     },{
       name: "Latte",
       image: "/new/path",
       oneSize: null,
-      smallPrice: 4.5,
-      mediumPrice: 5.5,
-      largePrice: 6.5
+      smallPrice: 3.95,
+      mediumPrice: 4.65,
+      largePrice: 5.15
     },{
       name: "Cappuccino",
       image: "/new/path",
       oneSize: null,
-      smallPrice: 4.5,
-      mediumPrice: 5.5,
-      largePrice: 6.5
+      smallPrice: 3.95,
+      mediumPrice: 4.65,
+      largePrice: 5.15
     },{
       name: "Americano",
       image: "/new/path",
       oneSize: null,
-      smallPrice: 4.5,
-      mediumPrice: 5.5,
-      largePrice: 6.5
+      smallPrice: 3.95,
+      mediumPrice: 4.65,
+      largePrice: 5.15
     },{
       name: "Espresso",
       image: "/new/path",
-      oneSize: 5.5,
+      oneSize: 2.65,
       smallPrice: null,
       mediumPrice: null,
       largePrice: null
     },{
       name: "Doppio",
       image: "/new/path",
-      oneSize: 5.5,
+      oneSize: 4.65,
       smallPrice: null,
       mediumPrice: null,
       largePrice: null
     },{
       name: "Cortado",
       image: "/new/path",
-      oneSize: 5.5,
+      oneSize: 5.65,
       smallPrice: null,
       mediumPrice: null,
       largePrice: null
@@ -53,121 +53,121 @@ const supplementData =
       name: "Red Eye",
       image: "/new/path",
       oneSize: null,
-      smallPrice: 4.5,
-      mediumPrice: 5.5,
-      largePrice: 6.5
+      smallPrice: 4.65,
+      mediumPrice: 5.65,
+      largePrice: 6.65
     },{
       name: "Galão",
       image: "/new/path",
       oneSize: null,
-      smallPrice: 4.5,
-      mediumPrice: 5.5,
-      largePrice: 6.5
+      smallPrice: 5.95,
+      mediumPrice: 6.45,
+      largePrice: 7.25
     },{
       name: "Lungo",
       image: "/new/path",
       oneSize: null,
-      smallPrice: 4.5,
-      mediumPrice: 5.5,
-      largePrice: 6.5
+      smallPrice: 4.95,
+      mediumPrice: 5.45,
+      largePrice: 6.45
     },{
       name: "Mocha",
       image: "/new/path",
       oneSize: null,
-      smallPrice: 4.5,
-      mediumPrice: 5.5,
-      largePrice: 6.5
+      smallPrice: 3.45,
+      mediumPrice: 4.15,
+      largePrice: 4.65
     },{
       name: "Macchiato",
       image: "/new/path",
       oneSize: null,
-      smallPrice: 4.5,
-      mediumPrice: 5.5,
-      largePrice: 6.5
+      smallPrice: 3.75,
+      mediumPrice: 4.45,
+      largePrice: 4.75
     },{
       name: "Ristretto",
       image: "/new/path",
       oneSize: null,
-      smallPrice: 4.5,
-      mediumPrice: 5.5,
-      largePrice: 6.5
+      smallPrice: 1.55,
+      mediumPrice: 2.55,
+      largePrice: 3.55
     },{
       name: "Flat White",
       image: "/new/path",
       oneSize: null,
-      smallPrice: 4.5,
-      mediumPrice: 5.5,
-      largePrice: 6.5
+      smallPrice: 3.75,
+      mediumPrice: 4.75,
+      largePrice: 5.55
     },{
       name: "Affogato",
       image: "/new/path",
       oneSize: null,
-      smallPrice: 4.5,
-      mediumPrice: 5.5,
-      largePrice: 6.5
+      smallPrice: 4.75,
+      mediumPrice: 5.95,
+      largePrice: 7.25
     },{
       name: "Café au Lait",
       image: "/new/path",
       oneSize: null,
-      smallPrice: 4.5,
-      mediumPrice: 5.5,
-      largePrice: 6.5
+      smallPrice: 3.45,
+      mediumPrice: 4.15,
+      largePrice: 4.95
     },{
       name: "Irish",
       image: "/new/path",
       oneSize: null,
-      smallPrice: 4.5,
-      mediumPrice: 5.5,
-      largePrice: 6.5
+      smallPrice: 3.45,
+      mediumPrice: 4.15,
+      largePrice: 4.95
     },{
       name: "Guayoyo",
       image: "/new/path",
       oneSize: null,
-      smallPrice: 4.5,
-      mediumPrice: 5.5,
-      largePrice: 6.5
+      smallPrice: 4.75,
+      mediumPrice: 5.95,
+      largePrice: 6.95
     },{
       name: "Iced Coffee",
       image: "/new/path",
       oneSize: null,
-      smallPrice: 4.5,
-      mediumPrice: 5.5,
-      largePrice: 6.5
+      smallPrice: 2.65,
+      mediumPrice: 3.65,
+      largePrice: 4.65
     },{
       name: "Iced Espresso",
       image: "/new/path",
-      oneSize: null,
-      smallPrice: 4.5,
-      mediumPrice: 5.5,
-      largePrice: 6.5
+      oneSize: 3.25,
+      smallPrice: null,
+      mediumPrice: null,
+      largePrice: null
     },{
       name: 'Cold Brew',
       image: "/new/path",
       oneSize: null,
-      smallPrice: 4.5,
-      mediumPrice: 5.5,
-      largePrice: 6.5
+      smallPrice: 3.65,
+      mediumPrice: 4.65,
+      largePrice: 5.65
     },{
       name: "Frappuccino",
       image: "/new/path",
       oneSize: null,
-      smallPrice: 4.5,
-      mediumPrice: 5.5,
-      largePrice: 6.5
+      smallPrice: 4.95,
+      mediumPrice: 5.25,
+      largePrice: 6.45
     },{
       name: "Nitro",
       image: "/new/path",
       oneSize: null,
-      smallPrice: 4.5,
-      mediumPrice: 5.5,
-      largePrice: 6.5
+      smallPrice: 4.25,
+      mediumPrice: 5.25,
+      largePrice: 6.45
     },{
       name: "Mazagran",
       image: "/new/path",
       oneSize: null,
-      smallPrice: 4.5,
-      mediumPrice: 5.5,
-      largePrice: 6.5
+      smallPrice: 5.95,
+      mediumPrice: 6.45,
+      largePrice: 7.25
     }
   ]
 
