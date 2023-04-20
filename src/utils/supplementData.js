4ba87e4e6e83015789c8e15659f4e1360fc2a2d8
Jeff Sahim <jeffreysahim@gmail.com>
2023-04-19 22:55:01 -0400

    Update supplementData images

--- src/utils/supplementData.js
+++ src/utils/supplementData.js
@@ -2,168 +2,168 @@ const supplementData =
   [
     {
       name: "Black",
-      image: "/new/path",
+      image: "https://lianaskitchen.co.uk/wp-content/uploads/Black-Coffee-720x540.jpg",
       oneSize: null,
       smallPrice: 2.65,
       mediumPrice: 3.65,
       largePrice: 4.65
     },{
       name: "Latte",
-      image: "/new/path",
+      image: "https://lianaskitchen.co.uk/wp-content/uploads/Latte-720x540.jpg",
       oneSize: null,
       smallPrice: 3.95,
       mediumPrice: 4.65,
       largePrice: 5.15
     },{
       name: "Cappuccino",
-      image: "/new/path",
+      image: "https://lianaskitchen.co.uk/wp-content/uploads/Cappuccino-720x540.jpg",
       oneSize: null,
       smallPrice: 3.95,
       mediumPrice: 4.65,
       largePrice: 5.15
     },{
       name: "Americano",
-      image: "/new/path",
+      image: "https://lianaskitchen.co.uk/wp-content/uploads/Americano--720x540.jpg",
       oneSize: null,
       smallPrice: 3.95,
       mediumPrice: 4.65,
       largePrice: 5.15
     },{
       name: "Espresso",
-      image: "/new/path",
+      image: "https://lianaskitchen.co.uk/wp-content/uploads/Espresso-720x540.jpg",
       oneSize: 2.65,
       smallPrice: null,
       mediumPrice: null,
       largePrice: null
     },{
       name: "Doppio",
-      image: "/new/path",
+      image: "https://lianaskitchen.co.uk/wp-content/uploads/Doppio-720x540.jpg",
       oneSize: 4.65,
       smallPrice: null,
       mediumPrice: null,
       largePrice: null
     },{
       name: "Cortado",
-      image: "/new/path",
+      image: "https://lianaskitchen.co.uk/wp-content/uploads/Cortado-720x540.jpg",
       oneSize: 5.65,
       smallPrice: null,
       mediumPrice: null,
       largePrice: null
     },{
       name: "Red Eye",
-      image: "/new/path",
+      image: "https://user-images.githubusercontent.com/107663888/233244561-1619125f-058d-4220-9334-68d8b00610f2.png",
       oneSize: null,
       smallPrice: 4.65,
       mediumPrice: 5.65,
       largePrice: 6.65
     },{
       name: "Galão",
-      image: "/new/path",
+      image: "https://user-images.githubusercontent.com/107663888/233244560-f417f081-3a44-4758-a6bb-8214ab49f699.png",
       oneSize: null,
       smallPrice: 5.95,
       mediumPrice: 6.45,
       largePrice: 7.25
     },{
       name: "Lungo",
-      image: "/new/path",
+      image: "https://lianaskitchen.co.uk/wp-content/uploads/Lungo-720x540.jpg",
       oneSize: null,
       smallPrice: 4.95,
       mediumPrice: 5.45,
       largePrice: 6.45
     },{
       name: "Mocha",
-      image: "/new/path",
+      image: "https://lianaskitchen.co.uk/wp-content/uploads/Mocha-720x540.jpg",
       oneSize: null,
       smallPrice: 3.45,
       mediumPrice: 4.15,
       largePrice: 4.65
     },{
       name: "Macchiato",
-      image: "/new/path",
+      image: "https://lianaskitchen.co.uk/wp-content/uploads/Macchiato-720x540.jpg",
       oneSize: null,
       smallPrice: 3.75,
       mediumPrice: 4.45,
       largePrice: 4.75
     },{
       name: "Ristretto",
-      image: "/new/path",
+      image: "https://lianaskitchen.co.uk/wp-content/uploads/Ristretto-720x540.jpg",
       oneSize: null,
       smallPrice: 1.55,
       mediumPrice: 2.55,
       largePrice: 3.55
     },{
       name: "Flat White",
-      image: "/new/path",
+      image: "https://lianaskitchen.co.uk/wp-content/uploads/Flat-White-Coffee-720x540.jpg",
       oneSize: null,
       smallPrice: 3.75,
       mediumPrice: 4.75,
       largePrice: 5.55
     },{
       name: "Affogato",
-      image: "/new/path",
+      image: "https://user-images.githubusercontent.com/107663888/233244556-98002005-6f85-4a78-be05-d656bf4821ce.png",
       oneSize: null,
       smallPrice: 4.75,
       mediumPrice: 5.95,
       largePrice: 7.25
     },{
       name: "Café au Lait",
-      image: "/new/path",
+      image: "https://lianaskitchen.co.uk/wp-content/uploads/Cafe-au-Lait-720x540.jpg",
       oneSize: null,
       smallPrice: 3.45,
       mediumPrice: 4.15,
       largePrice: 4.95
     },{
       name: "Irish",
-      image: "/new/path",
+      image: "https://lianaskitchen.co.uk/wp-content/uploads/Irish-Coffee-720x540.jpg",
       oneSize: null,
       smallPrice: 3.45,
       mediumPrice: 4.15,
       largePrice: 4.95
     },{
       name: "Guayoyo",
-      image: "/new/path",
+      image: "https://user-images.githubusercontent.com/107663888/233246281-1b6f86d4-e61b-4d2d-90e0-3bbef42e02b5.png",
       oneSize: null,
       smallPrice: 4.75,
       mediumPrice: 5.95,
       largePrice: 6.95
     },{
       name: "Iced Coffee",
-      image: "/new/path",
+      image: "https://lianaskitchen.co.uk/wp-content/uploads/Iced-Coffee-720x540.jpg",
       oneSize: null,
       smallPrice: 2.65,
       mediumPrice: 3.65,
       largePrice: 4.65
     },{
       name: "Iced Espresso",
-      image: "/new/path",
+      image: "https://user-images.githubusercontent.com/107663888/233244554-3b6a0fc8-1770-4e96-b14e-db073e31ba92.png",
       oneSize: 3.25,
       smallPrice: null,
       mediumPrice: null,
       largePrice: null
     },{
       name: 'Cold Brew',
-      image: "/new/path",
+      image: "https://lianaskitchen.co.uk/wp-content/uploads/Cold-Brew-Coffee-720x540.jpg",
       oneSize: null,
       smallPrice: 3.65,
       mediumPrice: 4.65,
       largePrice: 5.65
     },{
       name: "Frappuccino",
-      image: "/new/path",
+      image: "https://lianaskitchen.co.uk/wp-content/uploads/Frappuccino-720x540.jpg",
       oneSize: null,
       smallPrice: 4.95,
       mediumPrice: 5.25,
       largePrice: 6.45
     },{
       name: "Nitro",
-      image: "/new/path",
+      image: "https://lianaskitchen.co.uk/wp-content/uploads/Nitro-Coffee-720x540.jpg",
       oneSize: null,
       smallPrice: 4.25,
       mediumPrice: 5.25,
       largePrice: 6.45
     },{
       name: "Mazagran",
-      image: "/new/path",
+      image: "https://user-images.githubusercontent.com/107663888/233244553-df012bfe-6712-4ba0-90f3-3664ce0a65e1.png",
       oneSize: null,
       smallPrice: 5.95,
       mediumPrice: 6.45,
